325f7dcaee0404d74ba16d84c62f52e503b13165
Joao Farias <65802222+jpcfarias@users.noreply.github.com>
2022-04-12 11:04:28 -0300

    Add files via upload

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedro-2 <jpedro-2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 20:37:55 by jpedro-2          #+#    #+#             */
+/*   Updated: 2022/04/07 15:32:21 by jpedro-2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdbool.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_write_comb(char a, char b, char c, bool last)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (last)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+	bool	last;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				last = !(a == '7' && b == '8' && c == '9');
+				ft_write_comb(a, b, c, last);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpedro-2 <jpedro-2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 02:06:47 by jpedro-2          #+#    #+#             */
+/*   Updated: 2022/04/07 15:53:47 by jpedro-2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdbool.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_write_comb(int a, int b, bool last)
+{
+	ft_putchar(48 + a / 10);
+	ft_putchar(48 + a % 10);
+	ft_putchar(' ');
+	ft_putchar(48 + b / 10);
+	ft_putchar(48 + b % 10);
+	if (last)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	int		a;
+	int		b;
+	bool	last;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			last =! (a == 98 && b == 99);
+			ft_write_comb(a, b, last);
+			b++;
+		}
+		a++;
+	}
+}
